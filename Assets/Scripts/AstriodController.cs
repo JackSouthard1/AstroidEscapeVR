eb6538665f43f9ab08ef41bfbc0bc9e7c091e7ca
jacksouthard <jacksouthard265@gmail.com>
2018-12-11 20:11:13 -0800

    Add astroid models and randomizer

---
+++ Assets/Scripts/AstriodController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstriodController : MonoBehaviour {
+	public Mesh[] meshes;
+	public float scaleVariance;
+
+	void Start () {
+		transform.rotation = Random.rotation;
+		transform.localScale = new Vector3(1 + Random.Range(-scaleVariance, scaleVariance), 1 + Random.Range(-scaleVariance, scaleVariance), 1 + Random.Range(-scaleVariance, scaleVariance));
+
+		Mesh mesh = meshes[Random.Range(0, meshes.Length)];
+		GetComponent<MeshFilter>().mesh = mesh;
+		GetComponent<MeshCollider>().sharedMesh = mesh;
+	}
+}
