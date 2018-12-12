51c488199bb8abc699f51535dd014bb84f4432bf
jacksouthard <jacksouthard265@gmail.com>
2018-12-11 21:55:02 -0800

    Trass worm

---
+++ Assets/WormController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour {
+	Transform player;
+	public float startFollowDist;
+	public float maxFollowDist;
+	public float speed;
+
+	void Start () {
+		player = FindObjectOfType<PlayerController>().transform;
+	}
+	
+	void Update () {
+		float dist = (transform.position - player.position).magnitude;
+		float curSpeed = speed;
+		if (dist > maxFollowDist) {
+			curSpeed *= 10;
+		}
+		transform.LookAt(player);
+		transform.position += transform.forward * curSpeed * Time.deltaTime;
+	}
+}
